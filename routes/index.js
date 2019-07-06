956ee9f42879a3b2aee00317814cde0974670dac
Narasimha Reddy <macbook@APPLEs-MacBook-Air.local>
2019-07-06 11:40:36 +0530

    post method

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+//import model
+const Index_m = require('../models/index');
 
 router.get('/', (req,res)=>{
     res.render('index');
 });
 
+router.post('/', (req,res)=>{
+    const fdata = new Index_m();
+    fdata.fname = req.body.fullname;
+    fdata.email = req.body.email;
+    fdata.address = req.body.add;
+    fdata.password = req.body.pwd;
+    fdata.save((err)=>{
+        if(err){throw err}
+        else{res.send("data saved")}
+    });
+
+})
 
 
 
