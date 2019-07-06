659152bbc19a0d53a5ffe13baaa5e2ce3d007090
Narasimha Reddy <macbook@APPLEs-MacBook-Air.local>
2019-07-06 16:51:38 +0530

    react page setup

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const indexSchema = new Schema({
-    name : {type:String},
+    fname : {type:String},
     email : {type:String},
     password : {type:String},
     address : {type:String}
--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,5 +1,45 @@
 import React from 'react';
-import './App.css';
+
+class App extends React.Component{
+   
+    render(){
+        return(
+            <div className="container">
+                <div className="jumbotron jumbotron-fluid">
+                    <div className="form">
+                    </div>
+                    <div className="note text-center">
+                        <h2>React app</h2>
+                    </div>
+                    <form action="/" method="POST" >
+                        <div className="form-content">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <input type="text" name="fullname" className="form-control" placeholder="Your Name *"  />
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" name="add" className="form-control" placeholder="Address *" />
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <input type="text" name="email" className="form-control" placeholder="Your Email *" />
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" name="pwd" className="form-control" placeholder="Password *" />
+                                    </div>
+                                </div>
+                            </div>
+                            <button type="submit" className="btnSubmit">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
 
 
 export default App;
